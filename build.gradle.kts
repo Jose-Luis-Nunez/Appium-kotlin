609fd971f3f5d7675570d4f15bb845623029ee90
jose-luis-nunez <josenunez@outlook.de>
2024-04-17 08:28:16 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,25 +10,19 @@ repositories {
 }
 
 dependencies {
+    implementation("io.appium:java-client:7.5.1")
+
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.22")
-
-//    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-//    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-
-    implementation("io.appium:java-client:7.4.1")
-
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.22")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.22")
-
     implementation("io.insert-koin:koin-core:3.2.0")
 
-    testImplementation("io.insert-koin:koin-test:3.2.0") {
+    testImplementation("io.insert-koin:koin-test:3.5.0") {
         exclude(group = "org.jetbrains.kotlin", module = "kotlin-test-junit")
     }
 
-    implementation("org.testng:testng:7.6.0")//7.9.0 versuchen
+    implementation("org.testng:testng:7.9.0")
     implementation("io.strikt:strikt-core:0.34.1")
-
 }
 
 tasks.test {
